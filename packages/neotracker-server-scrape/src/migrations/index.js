7fb12045f2c868d162189d50bca4297e3e4afeac
Alex DiCarlo <alexdicarlo@gmail.com>
2018-04-16 16:47:20 +0000

    Fix skipped transfers
    fbshipit-source-id: 9fc13b2

--- packages/neotracker-server-scrape/src/migrations/index.js
+++ packages/neotracker-server-scrape/src/migrations/index.js
@@ -9,15 +9,15 @@ import processedNextIndexNotify from './processedNextIndexNotify';
 export type MigrationName =
   | 'resyncActions'
   | 'fixQLC-0'
-  | 'fixSkippedTransfers-1'
-  | 'resyncTransferCoins-0'
+  | 'fixSkippedTransfers-2'
+  | 'resyncTransferCoins-1'
   | 'updateIndices'
   | 'processedNextIndexNotify';
 
 export default [
   ['resyncActions', resyncActions],
   ['fixQLC-0', fixQLC],
-  ['fixSkippedTransfers-1', fixSkippedTransfers],
+  ['fixSkippedTransfers-2', fixSkippedTransfers],
   ['resyncTransferCoins-1', resyncTransferCoins],
   ['updateIndices', updateIndices],
   ['processedNextIndexNotify', processedNextIndexNotify],
