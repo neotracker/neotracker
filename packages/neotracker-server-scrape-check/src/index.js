2d0bc7379d982a4884efa15473ddd47dcc40f20c
Alex Fragapane <34611820+afragapane@users.noreply.github.com>
2018-06-08 23:52:26 +0000

    Add script to monitor db - blockchain inconsistencies (#82)
    fbshipit-source-id: 7bef4e8

---
+++ packages/neotracker-server-scrape-check/src/index.js
@@ -0,0 +1,6 @@
+/* @flow */
+export { default as scrapeCheck } from './scrapeCheck';
+export type {
+  Environment as ScrapeCheckEnvironment,
+  Options as ScrapeCheckOptions,
+} from './scrapeCheck';
