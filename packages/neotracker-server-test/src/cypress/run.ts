4d5484c7f4581ef79c050609bb44a0b1922290b2
Alex DiCarlo <alexdicarlo@gmail.com>
2018-07-06 18:46:51 +0000

    Increase web startup timeout
    fbshipit-source-id: 922f3e3

--- packages/neotracker-server-test/src/cypress/run.ts
+++ packages/neotracker-server-test/src/cypress/run.ts
@@ -96,7 +96,7 @@ const run = async ({ ci }: { readonly ci: boolean }) => {
   });
   mutableCleanup.push(createKillProcess(proc));
 
-  await checkReady('web', proc, port, { path: 'healthcheck', timeoutMS: 120000 });
+  await checkReady('web', proc, port, { path: 'healthcheck', timeoutMS: 300000 });
 
   // Wait for server to startup and sync
   await new Promise<void>((resolve) => setTimeout(resolve, 5000));
