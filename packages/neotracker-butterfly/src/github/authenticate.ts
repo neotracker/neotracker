3d05c55e46e053fa369a7dde390fe347c8fac20b
Alex DiCarlo <alexdicarlo@gmail.com>
2018-07-22 09:02:12 +0000

    Fix jwt signing for butterfly github
    fbshipit-source-id: bc4bc5e

--- packages/neotracker-butterfly/src/github/authenticate.ts
+++ packages/neotracker-butterfly/src/github/authenticate.ts
@@ -2,12 +2,19 @@ import * as jwt from 'jsonwebtoken';
 import { api } from './api';
 
 export const authenticate = ({ appID, privateKey }: { readonly appID: string; readonly privateKey: string }) => {
-  const nowSeconds = Math.round(Date.now() / 1000);
-  const payload = {
-    iat: nowSeconds,
-    exp: nowSeconds + 10 * 60,
-    iss: appID,
-  };
-  const token = jwt.sign(payload, privateKey);
-  api.authenticate({ type: 'app', token });
+  function doAuthentication(): void {
+    const nowSeconds = Math.round(Date.now() / 1000);
+    const payload = {
+      iat: nowSeconds,
+      exp: nowSeconds + 10 * 60,
+      iss: appID,
+    };
+    const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+    api.authenticate({ type: 'app', token });
+
+    // Keep the authentication up to date.
+    setTimeout(doAuthentication, 8 * 60 * 1000).unref();
+  }
+
+  doAuthentication();
 };
