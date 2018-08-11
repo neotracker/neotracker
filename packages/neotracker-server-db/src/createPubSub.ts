d2d1cdb2fddd2bf1eb7a59eb0662f5c133f2b724
Alex DiCarlo <alexdicarlo@gmail.com>
2018-08-11 05:12:11 +0000

    Drop pg-pubsub
    fbshipit-source-id: e495070

--- packages/neotracker-server-db/src/createPubSub.ts
+++ packages/neotracker-server-db/src/createPubSub.ts
@@ -1,10 +1,7 @@
 import { Monitor } from '@neo-one/monitor';
-// @ts-ignore
-import PGPubsub from 'pg-pubsub';
+import { Client, Notification } from 'pg';
 import { Observable, Observer, Subject } from 'rxjs';
 import { share } from 'rxjs/operators';
-
-import { createConnectionString } from './createConnectionString';
 
 export const PROCESSED_NEXT_INDEX = 'processed_next_index';
 
@@ -23,6 +20,8 @@ export interface PubSubOptions {
       readonly password?: string;
     };
   };
+  readonly maxAttempts?: number;
+  readonly reconnectTimeMS?: number;
 }
 export interface PubSubEnvironment {
   readonly host?: string;
@@ -40,45 +39,110 @@ const createPGPubSub = <T>({
   readonly options: PubSubOptions;
   readonly environment: PubSubEnvironment;
 }): PubSub<T> => {
-  const pgpubsub = new PGPubsub(
-    createConnectionString({
+  const { maxAttempts = 5, reconnectTimeMS = 5000 } = options;
+
+  let connected = false;
+  const createClient = () =>
+    new Client({
       ...environment,
       ...(options.db === undefined || options.db.connection === undefined ? {} : options.db.connection),
-      readWrite: true,
-    }),
-    {
-      // tslint:disable-next-line no-any
-      log: (...args: any[]) => {
-        if (args[0] != undefined && args[0] instanceof Error) {
-          monitor.logError({ name: 'pg_pubsub_error', error: args[0] });
-        } else {
-          monitor.log({ name: 'pg_pubsub', level: 'verbose' });
-        }
-      },
-    },
-  );
-  pgpubsub.on('error', (error: Error) => {
-    monitor.logError({ name: 'pg_pubsub_event_error', error });
-  });
+    });
+
+  let client = createClient();
+
+  const connectClient = async () => {
+    if (!connected) {
+      connected = true;
+      try {
+        await client.connect();
+      } catch (error) {
+        connected = false;
+        throw error;
+      }
+    }
+  };
+
+  const closeClient = async () => {
+    const currentClient = client;
+    client = createClient();
+    connected = false;
+    try {
+      currentClient.removeAllListeners();
+      await currentClient.end();
+    } catch (error) {
+      monitor.logError({ name: 'pg_pubsub_close_error', error });
+    }
+  };
+
+  const doCloseClient = () => {
+    closeClient()
+      .then(() => {
+        // do nothing
+      })
+      .catch(() => {
+        // do nothing
+      });
+  };
 
   const value$ = Observable.create((observer: Observer<T>) => {
-    const listener = (payload: T) => {
-      observer.next(payload);
+    const listener = (data: Notification) => {
+      observer.next(data.payload === undefined ? undefined : JSON.parse(data.payload));
     };
-    pgpubsub.addChannel(channel, listener);
+
+    let attempts = 0;
+    const listen = async () => {
+      try {
+        await closeClient();
+        await connectClient();
+        // tslint:disable-next-line no-any
+        (client as any).connection.stream.setKeepAlive(true);
+        await client.query(`LISTEN ${channel};`);
+
+        client.on('notification', listener);
+        client.on('error', (error) => {
+          monitor.logError({ name: 'pg_pubsub_client_error', error });
+          doListen();
+        });
+        client.on('end', () => {
+          monitor.logError({ name: 'pg_pubsub_client_end_error', error: new Error('Unexpected end') });
+          doListen();
+        });
+        attempts = 0;
+      } catch (error) {
+        monitor.logError({ name: 'pg_pubsub_error', error });
+        if (attempts >= maxAttempts) {
+          throw error;
+        }
+
+        attempts += 1;
+        await new Promise<void>((resolve) => setTimeout(resolve, reconnectTimeMS));
+        await listen();
+      }
+    };
+
+    const doListen = () => {
+      listen()
+        .then(() => {
+          // do nothing
+        })
+        .catch((error) => observer.error(error));
+    };
+
+    doListen();
 
     return () => {
-      pgpubsub.removeChannel(channel, listener);
+      doCloseClient();
     };
   }).pipe(share());
 
   return {
     next: async (value: T) => {
-      await pgpubsub.publish(channel, value);
+      await connectClient();
+      await client.query('SELECT pg_notify($1, $2);', [channel, JSON.stringify(value)]);
     },
     value$,
     close: () => {
-      pgpubsub.close();
+      doCloseClient();
     },
   };
 };
